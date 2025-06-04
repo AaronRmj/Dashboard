5b783d444b8cc6ea250e80325b84846c58cda303
gax50 <gaelrova32@gmail.com>
2025-06-04 14:05:47 +0200

    mis a jour front

--- src/components/ui/DynamicInput.jsx
+++ src/components/ui/DynamicInput.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const DynamicInput = ({ icon: Icon, name, placeholder,disabled, type, value, onChange, inputClassName }) => {
     return (
-        <div className="py-2 text-gray-700 text-md rounded-md relative flex items-center">
+        <div className="py-2 text-gray-700 text-lg rounded-md relative flex items-center">
             {/* si icon existe alors ... */}
-            {Icon && <Icon className="text-gray-500 mr-2 absolute pl-3 text-2xl"/>} 
+            {Icon && <Icon className="text-gray-500 mr-5 absolute pl-3 text-3xl -ml-1 " />} 
              
             <input      
                 type={type}
@@ -12,7 +12,7 @@ const DynamicInput = ({ icon: Icon, name, placeholder,disabled, type, value, onC
                 value={value}
                 name={name}
                 onChange={onChange}
-                className={`pl-7 ${inputClassName} p-[10px] border-1 w-3/4 border-gray-300 focus:border-gray-400 rounded-lg focus:outline-0`}
+                className={`pl-7 ${inputClassName} p-[8px]  border-1 w-4/3 border-gray-300   focus:outline-none focus:ring-1 focus:ring-sky-300 rounded-lg focus:outline-0`}
                 disabled={disabled}
             />
           
--- src/pages/auth/login/CreateAccount.jsx
+++ src/pages/auth/login/CreateAccount.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const CreateAccount = () =>{
-    return(
-        <p className="text-sm text-gray-500 text-thin">Don't have an account? <Link to="/signup"  className="text-blue-600 text-sm">Create an account</Link></p>
-    )
-}
 
+const CreateAccount = () => {
+  return (
+    <div className="flex justify-between items-center mt-4 w-full px-1 text-sm text-gray-800">
+      <p className="font-light">
+        Vous n’avez pas de compte ?{" "}
+        
+      </p><Link to="/signup" className="text-blue-600  hover:text-gray-500 hover:scale-102 transition-all duration-[600ms]">
+          Créer un compte
+        </Link>
+    
+    </div>
+  );
+};
 
-export default CreateAccount
+export default CreateAccount;
