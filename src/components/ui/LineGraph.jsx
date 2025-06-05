2a2afabd4cb361a229dfa4dd264b164601758092
senpai <aaronrmjk@gmail.com>
2025-06-05 23:27:11 +0300

    nanova an ilay chart tam light mode
    ilay dark mode lony najanona mitsy fa ratsy be ilay design

--- src/components/ui/LineGraph.jsx
+++ src/components/ui/LineGraph.jsx
@@ -1,60 +1,113 @@
 //importer les composants necessaires
-import {Chart as ChartJS, CategoryScale, LinearScale, PointElement,LineElement, Title, Tooltip, Legend} from "chart.js";
-
-import {Line} from "react-chartjs-2";
-
-//enregistrer les composants necessaires de chart.js
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
+import {Chart as ChartJS, 
+    LineElement, 
+    PointElement, 
+    LinearScale, 
+    CategoryScale, 
     Tooltip, 
-    Legend
-);
-//definitions des données a utiliser pour le graph
-const data = {
-    labels:[
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday"
-    ],
-    datasets:[
-        {
-            data:[0,6000,9000,4000,1000,5000,9000],
-            backgroundColor: "rgba(29,78,216,1)",
-            borderColor:"#45e6d5",
-            pointBackgroundColor: "#000",
-            tension: 0.4,
+    Filler,
+    Title } from "chart.js";
+  
+  import {Line} from "react-chartjs-2";
+  
+  //enregistrer les composants necessaires de chart.js
+  ChartJS.register(
+    LineElement, 
+    PointElement, 
+    LinearScale, 
+    CategoryScale, 
+    Tooltip, 
+    Filler,
+    Title
+  );
+  //definitions des données a utiliser pour le graph
+  const LineGraph = () =>{
+  const data = {
+    labels: ['10:04', '8:44', '4:06', '7:08', '0:00'], 
+    datasets: [
+      {
+        label: 'Bénefice net',
+        data: [400, 350, 200, 100, 0],
+        tension: 0.4,
+        borderWidth: 1,
+        borderColor: '#3B82F6',
+        backgroundColor: 'rgba(59, 130, 246, 0.1)',
+        pointBackgroundColor: '#3B82F6',
+        pointRadius: 4,
+        pointHoverRadius: 6,
+        fill: {
+          target: 'origin',
+          above: 'rgba(59, 130, 246, 0.2)'
         },
-        {
-            data:[1000,2000,6000,9000,4000,6000,0],
-            backgroundColor: "rgba(29,78,216,1)",
-            borderColor: "red",
-            tension: 0.4,
-        }
+      },
+      {
+        label: 'Chiffre d affaire',
+        data: [100, 300, 0, 100, 500],
+        tension: 0.4,
+        borderWidth: 1,
+        borderColor: '#8B5CF6',
+        backgroundColor: 'rgba(139, 92, 246, 0.8)',
+        pointBackgroundColor: '#3B82F6',
+        pointRadius: 4,
+        pointHoverRadius: 6,
+        fill: {
+          target: 'origin',
+          above: 'rgba(139, 92, 246, 0.2)'
+        },
+      }
+  
     ]
-};
-
-const options = {
+  };
+  ;
+  
+  const options = {
     responsive: true,
-    plugins:{
-        legend:{
-            position:top,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: true,
+        position:"top",
+  
+      },
+      tooltip: {
+        enabled: true,
+        mode: 'index',
+        intersect: true,
+      },
+      title: {
+        display: true, // Désactive le titre intégré si nécessaire
+      }
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+          drawBorder: false
         },
-        title:{
-            display:true,
-            text:"Bénéfice en fonction du temps"
+        ticks: {
+          color: '#94A3B8'
         }
+      },
+      y: {
+        beginAtZero: true,
+        min: 0,
+        max: 500,
+        grid: {
+          color: 'rgba(255, 255, 255, 0.05)',
+          borderDash: [3, 3],
+          drawBorder: false
+        },
+        ticks: {
+          stepSize: 100,
+          color: '#94A3B8',
+          // callback: function(value) {
+          //   return value === 0 ? '0' : value + 'K';
+          // }
+        }
+      }
     }
-}
-
-const LineGraph = () =>{
-    return <Line options={options} data={data} />
-};
-export {LineGraph}
+  };
+  
+      return <Line options={options} data={data} />
+  };
+  export {LineGraph}
