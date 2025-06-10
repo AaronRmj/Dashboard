94259ba8bd2e6178a3510f7a171b2e44d0c41c87
senpai <aaronrmjk@gmail.com>
2025-06-11 00:37:31 +0300

    Update Dashboard.jsx

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -9,10 +9,10 @@ import BeneficeChart from "../../components/ui/BeneficeChart";
 const Dashboard = () =>{
 
     const Statistics = [
-        {title: "Nombre de clients", icon: <IoPeopleOutline /> , number: "300"},
-        {title: "Nombre de commandes", icon: <MdProductionQuantityLimits />  , number: "500"},
-        {title: "Total achats", icon:<FcSalesPerformance />  , number: "15M"},
-        {title: "Total ventes", icon:<GoPackageDependencies /> , number: "40M"},
+        {title: "Nombre de clients", icon: <IoPeopleOutline /> , number: "3000"},
+        {title: "Nombre de commandes", icon: <MdProductionQuantityLimits />  , number: "5000"},
+        {title: "Total achats", icon:<FcSalesPerformance />  , number: "$10000"},
+        {title: "Total ventes", icon:<GoPackageDependencies /> , number: "$400"},
         
     ]
 
@@ -22,7 +22,7 @@ const Dashboard = () =>{
             <div className="mx-auto lg:space-x-7 mb-6">
                 <Stats Statistics={Statistics} />
             </div>
-            <div className="flex-1 bg-white rounded-xl shadow-xl px-5">
+            <div className="flex-1 h-2/3 bg-white rounded-xl shadow-xl px-5">
                 <BeneficeChart />
             </div>
         </div>
